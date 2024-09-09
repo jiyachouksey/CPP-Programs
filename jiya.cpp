df912c45af6a9d9c8aa4efb30f52bffc633b80eb
jiyachouksey <162309021+jiyachouksey@users.noreply.github.com>
2024-09-09 22:28:40 +0530

    Addition of two numbers in cpp

---
+++ jiya.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter number a =";
+    cin>>a;
+    cout<<"enter number b =";
+    cin>>b;
+    int sum = a+b;
+    cout<<"sum :" <<sum<<endl;
+    return 0;
+}
