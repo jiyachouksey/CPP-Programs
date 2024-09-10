a872625b63c1c3a27cd27164f62b801d01ea7aaf
jiyachouksey <162309021+jiyachouksey@users.noreply.github.com>
2024-09-10 15:26:10 +0530

    positive number and negative number

---
+++ positive.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number n =";
+    cin>>n;
+    if(n>=0){
+        cout<<"number is positive"<<endl;
+     }
+     else{
+        cout<<"number is negative"<<endl;
+     }
+     return 0;
+
+}
